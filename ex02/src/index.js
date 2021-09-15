180a57307cc5be7517bdfa06608cb51bdd9e1f26
Zekerijah <zekerijahsehic1@gmail.com>
2021-09-15 14:35:39 +0200

    Commenting

---
+++ ex02/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+    // Only change code below this line
+
+    //comme
+
+    /* comme
+        nt */
+        
+    // Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
